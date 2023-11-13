55b7d06bacb1148cd601d12c5d2fd2ecd1cb9f62
Shakhabutdinov <uabdulaz038@gmail.com>
2023-11-13 17:18:47 +0500

    img scroll added

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function showMenuBar() {
 function hideMenuBar() {
   navLinks.style.right = "-200px"
 }
+
+const initSlider = () => {
+  const imgList = document.querySelector(".slider-wrapper .img-list");
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  slideButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "previous-img" ? -1 : 1;
+      const scrollAmount = imgList.clientWidth * direction;
+      imgList.scrollBy({left:scrollAmount, behaviour: "smooth"});
+    });
+  });
+}
+
+window.addEventListener("load", initSlider)
