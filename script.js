c6558e1a9244d14c5f36e0af3c6eb577aec5c7db
Shakhabutdinov <uabdulaz038@gmail.com>
2023-11-14 17:59:10 +0500

    commit

--- script.js
+++ script.js
@@ -9,12 +9,29 @@ function hideMenuBar() {
 const initSlider = () => {
   const imgList = document.querySelector(".slider-wrapper .img-list");
   const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  const sliderScroll = document.querySelector(".container .slider-scroll");
+  const maxScrollLeft = imgList.scrollWidth - imgList.clientWidth;
   slideButtons.forEach(button => {
     button.addEventListener("click", () => {
-      const direction = button.id === "previous-img" ? -1 : 1;
+      const direction = button.id === "prev-img" ? -1 : 1;
       const scrollAmount = imgList.clientWidth * direction;
-      imgList.scrollBy({left:scrollAmount, behaviour: "smooth"});
+      imgList.scrollBy({left: scrollAmount, behaviour: "smooth"});
     });
+  });
+
+  const handleButtons = () => {
+    slideButtons[0].style.display = imgList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display = imgList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  }
+
+  const updateScroll = () => {
+    const ScrollPosition = imgList.scrollLeft;
+    const Thumb = (ScrollPosition / maxScrollLeft) * ()
+  }
+
+  imgList.addEventListener("scroll", () => {
+    handleButtons();
+    updateScroll();
   });
 }
 
